0617ccdc617da9a37ddf894d6e1c5b23f3035a17
Mateus <mateussp2005@gmail.com>
2025-10-23 11:23:29 -0300

    Adiciona classe  NodeAvl.java

---
+++ src/edb2/arvores/busca/NodeAvl.java
@@ -0,0 +1,17 @@
+package edb2.arvores.busca;
+
+public class NodeAvl {
+	
+	int value;
+	NodeAvl left;
+	NodeAvl right;
+
+
+	public NodeAvl(int valor) {
+		
+		this.left = null;
+		this.right = null;
+		this.value = valor ;
+		
+	}
+}
