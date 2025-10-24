700a638445e206379e69497d4884137ae56a60fc
Mateus <mateussp2005@gmail.com>
2025-10-24 20:37:58 -0300

    Adiciona rotações e inserção AVL

--- src/edb2/arvores/busca/AvlTree.java
+++ src/edb2/arvores/busca/AvlTree.java
@@ -13,8 +13,16 @@ public class AvlTree {
 	public void setInicio(NodeAvl root) {
 		this.root = root;
 	}
+	
+	
 
-	//Busca de arvore binária
+	public AvlTree() {
+		super();
+		this.root = null;
+	}
+
+
+	//Busca de árvore binária
 	
 	public NodeAvl searchValue(NodeAvl node, int value){
 		
@@ -50,9 +58,112 @@ public class AvlTree {
 			node.altura = Math.max(alturaNode(node.left), alturaNode(node.right)) + 1;
 			
 		}
+	}
+	
+	public NodeAvl rotateLeft(NodeAvl node) {
 		
+		NodeAvl nodeRight = node.right;
+		
+		NodeAvl nodeRL = nodeRight.left;
+		
+		nodeRight.left = node;
+		
+		node.right = nodeRL;
+		
+		updateAltura(node);
+		
+		updateAltura(nodeRight);
+		
+		return nodeRight;
 		
 	}
+	
+	
+	
+	public NodeAvl rotateRight(NodeAvl node) {
+		
+		NodeAvl nodeLeft = node.left;
+		
+		NodeAvl nodeLR = nodeLeft.right;
+		
+		nodeLeft.right = node;
+		
+		node.left = nodeLR;
+		
+		updateAltura(node);
+		
+		updateAltura(nodeLeft);
+		
+		return nodeLeft;
+		
+	}
+	
+	public NodeAvl rotateRL(NodeAvl node){
+		
+		node.right = rotateRight(node.right);
+		node = rotateLeft(node);
+		
+		return node;
+	}
+	
+	public NodeAvl rotateLR(NodeAvl node) {
+		
+		node.left =  rotateLeft(node.left);
+		node = rotateRight(node);
+		
+		return node;
+		
+	}
+	
+	
+	public NodeAvl insertValue(NodeAvl node, int value) {
+		
+		if(node == null) return new NodeAvl(value);
+		
+		else{
+			if(value < node.value) {
+				
+				node.left = insertValue(node.left, value);
+				
+				if(alturaNode(node.left) - alturaNode(node.right) == 2) {
+					
+					if(value < node.left.value) node = rotateRight(node);
+					
+					else {
+						node = rotateLR(node);
+					}
+				}
+			}
+			else{
+				node.right = insertValue(node.right, value);
+				
+				if(alturaNode(node.right) - alturaNode(node.left) == 2) {
+					
+					if(value > node.right.value) node = rotateLeft(node);
+					
+					else {
+						node = rotateRL(node);
+					}
+				}
+			}
+			updateAltura(node);
+		}
+		return node;
+	}
+	
+    void printTree(NodeAvl node){
+    	
+        if(node == null){
+            System.out.println("\nA árvore está vazia");
+            return;
+        }
+
+        if(node.left != null) printTree(node.left);
+        
+        System.out.print(node.value + " ");
+        
+        if(node.right != null) printTree(node.right);
+    }
 
 
 }
