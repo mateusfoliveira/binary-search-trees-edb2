296147b8362198626fc7eb79bb98070a69105655
Mateus <mateussp2005@gmail.com>
2025-10-24 20:44:52 -0300

    Refactor: Encapsula a classe NodeAvl para dentro de AvlTree

--- src/edb2/arvores/busca/AvlTree.java
+++ src/edb2/arvores/busca/AvlTree.java
@@ -1,5 +1,23 @@
 package edb2.arvores.busca;
 
+class NodeAvl {
+	
+	int altura;
+	int value;
+	NodeAvl left;
+	NodeAvl right;
+
+
+	public NodeAvl(int valor) {
+		
+		this.left = null;
+		this.right = null;
+		this.value = valor ;
+		this.altura = 1;
+		
+	}
+}
+
 public class AvlTree {
 	
 	private NodeAvl root;
